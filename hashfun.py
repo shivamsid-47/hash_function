bc5d8f6e25492348910773ce400f2b0fe7ba8f4e
shivamsid-47 <sidharthshivam7@gmail.com>
2024-09-07 19:22:49 +0530

    Add files via upload

---
+++ hashfun.py
@@ -0,0 +1,12 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # initializing objects
+... int_val = 4
+... str_val = 'ShivamSidharth'
+... flt_val = 24.56
+... 
+... # Printing the hash values.
+... # Notice Integer value doesn't change
+... # You'll have answer later in article.
+... print("The integer hash value is : " + str(hash(int_val)))
+... print("The string hash value is : " + str(hash(str_val)))
